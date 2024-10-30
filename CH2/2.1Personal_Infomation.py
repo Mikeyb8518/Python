c8c2da8645c6e18e5be0db91d8fc747895319f15
Mikeyb8518 <110793646+Mikeyb8518@users.noreply.github.com>
2024-10-29 22:53:01 -0700

    Add files via upload

---
+++ CH2/2.1Personal_Infomation.py
@@ -0,0 +1,25 @@
+#variable to hold name
+name = input('What is your name: ')
+
+#address variable to  hold address
+address =  input('What is your address: ')
+
+#city variable to  hold city
+city =  input('What is your city: ')
+
+#state variable to hold state
+state =  input('What is your state: ')
+
+#phone variable to hold phone number
+phone =   input('What is your phone number: ')
+
+#major variable to hold college major
+major = input('What is your college major: ')
+
+
+#Display results
+print('Name: ', name)
+print('Address: ', address)
+print(city, state)
+print('Phone: ', phone)
+print('College Major is:  ', major)
